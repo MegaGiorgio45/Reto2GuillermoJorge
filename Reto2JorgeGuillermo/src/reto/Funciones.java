55e76eec950bb97690668ee752b45ad782e2a0e5
MegaGiorgio45 <Jorge@PO-JORGE>
2025-11-18 11:44:21 +0100

    Merge branch 'main' of https://github.com/MegaGiorgio45/Reto2GuillermoJorge.git into main

--- Reto2JorgeGuillermo/src/reto/Funciones.java
+++ Reto2JorgeGuillermo/src/reto/Funciones.java
@@ -1,5 +1,21 @@
 package reto;
 
-public class Funciones {
+import java.util.Scanner;
 
+public class Funciones {
+	public static double dimeDouble(String texto, Scanner sc)
+	{
+		do
+		{
+		try {
+			System.out.println(texto);
+			String s = sc.nextLine();
+			double d= Double.parseDouble(s);
+			return d;
+
+		} catch (Exception e) {
+			System.out.println("Formato incorrecto. No es un número");
+		}
+		}while(true);
+	}
 }
--- Reto2JorgeGuillermo/src/reto/Funciones1.java
+++ Reto2JorgeGuillermo/src/reto/Funciones1.java
@@ -2,4 +2,18 @@ package reto;
 
 public class Funciones1 {
 
+	public static double celsius(double x) {
+		return (x * (9 / 5)) + 32;
+	}
+
+	public static double fahrenheit(double x) {
+		return (x - 32) * 5 / 9;
+	}
+	
+	public static double kilometro(double x) {
+		return x/1.609;
+	}
+	public static double milla(double x) {
+		return x*1.609;
+	}
 }
