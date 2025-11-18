7771c2ea0e59fbc9b7231de33898a0a7ffb742e9
guillemdb1994-pixel <Guille@Laptop-GMB>
2025-11-18 11:57:03 +0100

    qwe

--- Reto2JorgeGuillermo/src/reto/Funciones2.java
+++ Reto2JorgeGuillermo/src/reto/Funciones2.java
@@ -1,5 +1,16 @@
 package reto;
 
 public class Funciones2 {
-
+	public static double celsiusFahrenhait(double x) {
+		return (x*9/5) + 32;
+	}
+	public static double FahrenhaitCelsius(double x) {
+		return 1/celsiusFahrenhait(x);
+	}
+	public static double litrosGalones(double x) {
+		return x*0.26417;
+	}
+	public static double galonesLitros(double x) {
+		return 1/litrosGalones(x);
+	}
 }
